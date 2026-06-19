eeace80f0f714f7c3f3bbb0e3eced2d73a8cd658
wallacemaxters <wallacemaxters@gmail.com>
2026-06-19 15:21:27 -0300

    banner evento

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 <x-layouts.main>
     <x-sections.home.hero />
     <x-sections.home.quem-somos />
-    <x-sections.home.evento-especial />
     <x-sections.home.revista />
     <x-sections.home.autor />
     <x-sections.home.assinante />
